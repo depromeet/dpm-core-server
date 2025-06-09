000fdc7ca68058f75c5512fe0f7066d74e70f5dd
junwon <67488973+wjdwnsdnjs13@users.noreply.github.com>
2025-06-09 23:52:49 +0900

    Feat : swagger 추가 (#5)
    * build : spring doc 의존성 추가
    
    * feat : Swagger Config 추가
    
    * refactor : 리뷰 반영(도메인에 속하지 않는 것은 common 내부로)

---
+++ src/main/kotlin/com/server/dpmcore/common/configuration/SwaggerConfig.kt
@@ -0,0 +1,24 @@
+package com.server.dpmcore.common.configuration
+
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SwaggerConfig {
+
+    @Bean
+    fun OpenApi(): OpenAPI {
+        return OpenAPI()
+            .info(swaggerInfo())
+    }
+
+    private fun swaggerInfo(): Info {
+        return Info()
+            .version("v0.0.1")
+            .title("디프만 코어")
+            .description("디프만 코어 API 문서입니다.")
+    }
+
+}
