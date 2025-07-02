6e1648cdd359cfa947f1cd2f214836241f02b0b8
leehaneum <leehaneum160924@kyonggi.ac.kr>
2025-07-02 15:13:21 +0900

    fix: MemberCohort 참조 식별자 밸류 타입으로 변경

--- src/main/kotlin/com/server/dpmcore/member/memberCohort/domain/MemberCohort.kt
+++ src/main/kotlin/com/server/dpmcore/member/memberCohort/domain/MemberCohort.kt
@@ -1,7 +1,10 @@
 package com.server.dpmcore.member.memberCohort.domain
 
+import com.server.dpmcore.cohort.domain.CohortId
+import com.server.dpmcore.member.member.domain.MemberId
+
 data class MemberCohort(
     val id: MemberCohortId,
-    val memberId: String,
-    val cohortId: String,
+    val memberId: MemberId,
+    val cohortId: CohortId,
 )
