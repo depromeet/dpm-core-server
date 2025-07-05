a9024bd33af0a7191afe91e2bac3c07b997a194a
junwon <wjdwnsdnjs13@naver.com>
2025-07-05 23:07:16 +0900

    feat : 정산 Entity 추가

---
+++ src/main/kotlin/com/server/dpmcore/bill/bill/infrastructure/entity/BillEntity.kt
@@ -0,0 +1,32 @@
+package com.server.dpmcore.bill.bill.infrastructure.entity
+
+import com.server.dpmcore.bill.billAccount.infrastructure.entity.BillAccountEntity
+import com.server.dpmcore.gathering.gathering.infrastructure.entity.GatheringEntity
+import jakarta.persistence.*
+import java.time.Instant
+
+@Entity
+@Table(name = "bills")
+class BillEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bill_id", nullable = false, updatable = false)
+    val id: Long = 0,
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "bill_account_id", nullable = false)
+    val billAccount: BillAccountEntity,
+
+    @OneToMany(mappedBy = "bill", cascade = [CascadeType.ALL], orphanRemoval = true)
+    val gatherings: MutableList<GatheringEntity> = mutableListOf(),
+
+    @Column(name = "created_at", nullable = false)
+    val createdAt: Instant,
+
+    @Column(name = "updated_at", nullable = false)
+    val updatedAt: Instant,
+
+    @Column(name = "completed_at")
+    val completedAt: Instant? = null
+)
