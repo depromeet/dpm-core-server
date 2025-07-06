e90b4cae93b4e76345d5aae0254aa0a0a81d3364
its-sky <smc9919@naver.com>
2025-07-06 15:39:06 +0900

    chore: AttendanceRepository @Repository 어노테이션 누락된 것 정정 (#25)

--- src/main/kotlin/com/server/dpmcore/attendance/infrastructure/repository/AttendanceRepository.kt
+++ src/main/kotlin/com/server/dpmcore/attendance/infrastructure/repository/AttendanceRepository.kt
@@ -1,7 +1,9 @@
 package com.server.dpmcore.attendance.infrastructure.repository
 
 import com.server.dpmcore.attendance.domain.port.outbound.AttendancePersistencePort
+import org.springframework.stereotype.Repository
 
+@Repository
 class AttendanceRepository(
     private val attendanceJpaRepository: AttendanceJpaRepository,
 ) : AttendancePersistencePort
