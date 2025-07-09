2dbd6697a8bb1356737a822d0dd7ed2f5f430386
its-sky <smc9919@naver.com>
2025-07-10 01:48:29 +0900

    chore: 세션 조회 서비스 명칭 변경 (#28)

--- src/main/kotlin/com/server/dpmcore/session/application/SessionQueryService.kt
+++ src/main/kotlin/com/server/dpmcore/session/application/SessionQueryService.kt
@@ -11,7 +11,7 @@ import java.time.Instant
 
 @Service
 @Transactional(readOnly = true)
-class SessionReadService(
+class SessionQueryService(
     private val sessionPersistencePort: SessionPersistencePort,
 ) {
     fun getNextSession(): Session? {
