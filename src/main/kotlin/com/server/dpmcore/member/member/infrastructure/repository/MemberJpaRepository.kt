b1626657ac3f194ab8fdbffd56d779795a7df0e4
leehaneum <leehaneum160924@kyonggi.ac.kr>
2025-07-13 22:14:06 +0900

    refactor: 컨벤션에 따라 블럭 제거

--- src/main/kotlin/com/server/dpmcore/member/member/infrastructure/repository/MemberJpaRepository.kt
+++ src/main/kotlin/com/server/dpmcore/member/member/infrastructure/repository/MemberJpaRepository.kt
@@ -3,6 +3,4 @@ package com.server.dpmcore.member.member.infrastructure.repository
 import com.server.dpmcore.member.member.infrastructure.entity.MemberEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface MemberJpaRepository : JpaRepository<MemberEntity, Long> {
-
-}
+interface MemberJpaRepository : JpaRepository<MemberEntity, Long>
--- src/main/kotlin/com/server/dpmcore/refreshToken/infrastructure/repository/RefreshTokenJpaRepository.kt
+++ src/main/kotlin/com/server/dpmcore/refreshToken/infrastructure/repository/RefreshTokenJpaRepository.kt
@@ -3,5 +3,4 @@ package com.server.dpmcore.refreshToken.infrastructure.repository
 import com.server.dpmcore.refreshToken.infrastructure.entity.RefreshTokenEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface RefreshTokenJpaRepository : JpaRepository<RefreshTokenEntity, Long> {
-}
+interface RefreshTokenJpaRepository : JpaRepository<RefreshTokenEntity, Long>
