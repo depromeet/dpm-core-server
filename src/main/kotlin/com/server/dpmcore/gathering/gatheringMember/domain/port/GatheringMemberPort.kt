76bd651a6aa61262ec268b10dad45dd8f05da024
junwon <67488973+wjdwnsdnjs13@users.noreply.github.com>
2025-07-14 21:48:43 +0900

    refactor : 코드 포맷

--- src/main/kotlin/com/server/dpmcore/gathering/gatheringMember/domain/port/GatheringMemberPort.kt
+++ src/main/kotlin/com/server/dpmcore/gathering/gatheringMember/domain/port/GatheringMemberPort.kt
@@ -2,7 +2,6 @@ package com.server.dpmcore.gathering.gatheringMember.domain.port
 
 import com.server.dpmcore.gathering.gatheringMember.domain.model.GatheringMember
 
-
 interface GatheringMemberPort {
     fun save(gatheringMember: GatheringMember): GatheringMember
 }
