ddd5897b5503dde4050cbd3aba1abc601108c0b9
leehaneum <leehaneum160924@kyonggi.ac.kr>
2025-07-16 23:25:24 +0900

    feat: 스웨거 문서 작성

--- src/main/kotlin/com/server/dpmcore/member/member/presentation/MemberApi.kt
+++ src/main/kotlin/com/server/dpmcore/member/member/presentation/MemberApi.kt
@@ -4,10 +4,43 @@ import com.server.dpmcore.common.exception.CustomResponse
 import com.server.dpmcore.member.member.domain.model.MemberId
 import com.server.dpmcore.member.member.presentation.response.MemberDetailsResponse
 import com.server.dpmcore.security.annotation.CurrentMemberId
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.ExampleObject
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 
 @Tag(name = "Member", description = "멤버 API")
 interface MemberApi {
+    @ApiResponse(
+        responseCode = "200",
+        description = "로그인 한 멤버 조회 성공",
+        content = [
+            Content(
+                mediaType = APPLICATION_JSON_VALUE,
+                schema = Schema(implementation = CustomResponse::class),
+                examples = [
+                    ExampleObject(
+                        name = "로그인 한 멤버 조회 성공 응답",
+                        value = """
+                            {
+                                "status": "OK",
+                                "code": "G000",
+                                "message": "요청에 성공했습니다",
+                                "data": {
+                                    "email": "depromeetcore@gmail.com",
+                                    "name": "디프만"
+                                }
+                            }
+                        """,
+                    ),
+                ],
+            ),
+        ],
+    )
+    @Operation(summary = "로그인 한 멤버 조회 API", description = "현재 로그인한 멤버의 기본 정보를 조회 합니다.")
     fun me(
         @CurrentMemberId memberId: MemberId,
     ): CustomResponse<MemberDetailsResponse>
