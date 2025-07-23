0f65a4076e43442f103b3262eea760c94cf9f08f
junwon <67488973+wjdwnsdnjs13@users.noreply.github.com>
2025-07-23 09:01:43 +0900

    feat : 영수증 조회 구현

--- src/main/kotlin/com/server/dpmcore/gathering/gatheringReceipt/infrastructure/repository/ReceiptRepository.kt
+++ src/main/kotlin/com/server/dpmcore/gathering/gatheringReceipt/infrastructure/repository/ReceiptRepository.kt
@@ -3,6 +3,7 @@ package com.server.dpmcore.gathering.gatheringReceipt.infrastructure.repository
 import com.server.dpmcore.gathering.gatheringReceipt.domain.model.Receipt
 import com.server.dpmcore.gathering.gatheringReceipt.domain.port.ReceiptPersistencePort
 import com.server.dpmcore.gathering.gatheringReceipt.infrastructure.entity.ReceiptEntity
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -12,4 +13,9 @@ class ReceiptRepository(
     override fun save(receipt: Receipt) {
         receiptJpaRepository.save(ReceiptEntity.from(receipt))
     }
+
+    override fun findBy(gatheringReceiptId: Long): ReceiptEntity =
+        receiptJpaRepository.findByIdOrNull(
+            gatheringReceiptId,
+        ) ?: throw IllegalArgumentException("Receipt not found with id: $gatheringReceiptId")
 }
