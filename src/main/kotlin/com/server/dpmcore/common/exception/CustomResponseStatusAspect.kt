9b7da1907d876a6dafe6e2415a06181a5a3ac937
its-sky <smc9919@naver.com>
2025-07-23 20:22:59 +0900

    feat: CodeRabbit 코드리뷰 반영 (#71)

--- src/main/kotlin/com/server/dpmcore/common/exception/CustomResponseStatusAspect.kt
+++ src/main/kotlin/com/server/dpmcore/common/exception/CustomResponseStatusAspect.kt
@@ -3,13 +3,15 @@ package com.server.dpmcore.common.exception
 import jakarta.servlet.http.HttpServletResponse
 import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
 import org.springframework.stereotype.Component
 
+@Aspect
 @Component
 class CustomResponseStatusAspect(
     private val response: HttpServletResponse,
 ) {
-    @Around("within(* com.server.dpmcore..*Controller.*(..))")
+    @Around("within(com.server.dpmcore..*Controller)")
     @Throws(Throwable::class)
     fun handleResponseStatus(joinPoint: ProceedingJoinPoint): Any? {
         val result = joinPoint.proceed()
