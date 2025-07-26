c0d48cb289fd2501a1c18331be6d963f10ce1724
junwon <wjdwnsdnjs13@naver.com>
2025-07-26 21:43:12 +0900

    refactor : 정산 초대 멤버 기본 값 설정

--- src/main/kotlin/com/server/dpmcore/bill/bill/presentation/dto/response/BillListInviteAuthorityDetailResponse.kt
+++ src/main/kotlin/com/server/dpmcore/bill/bill/presentation/dto/response/BillListInviteAuthorityDetailResponse.kt
@@ -3,5 +3,21 @@ package com.server.dpmcore.bill.bill.presentation.dto.response
 data class BillListInviteAuthorityDetailResponse(
     val invitedAuthorityId: Long,
     val authorityName: String,
-    val authorityMemberCount: Long,
-)
+    val authorityMemberCount: Int,
+) {
+    companion object {
+        fun defaultInviteAuthorityResponse(): List<BillListInviteAuthorityDetailResponse> =
+            listOf(
+                BillListInviteAuthorityDetailResponse(
+                    invitedAuthorityId = 1L,
+                    authorityName = "17기 운영진",
+                    authorityMemberCount = 10,
+                ),
+                BillListInviteAuthorityDetailResponse(
+                    invitedAuthorityId = 2L,
+                    authorityName = "17기 디퍼",
+                    authorityMemberCount = 60,
+                ),
+            )
+    }
+}
--- src/main/kotlin/com/server/dpmcore/bill/bill/presentation/dto/response/CreateInviteAuthorityResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.server.dpmcore.bill.bill.presentation.dto.response
-
-data class CreateInviteAuthorityResponse(
-    val inviteAuthorityId: Long = 1L,
-    val authorityName: String = "17기",
-    val authorityMemberCount: Long = 1L,
-)
