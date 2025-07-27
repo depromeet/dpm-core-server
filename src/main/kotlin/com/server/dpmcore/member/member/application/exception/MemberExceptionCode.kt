67107f18217b1d6344eda7f776b9f266ccfb8f4a
its-sky <smc9919@naver.com>
2025-07-27 23:34:20 +0900

    chore: 빌드 에러 해결 (#89)

--- src/main/kotlin/com/server/dpmcore/member/member/application/exception/MemberExceptionCode.kt
+++ src/main/kotlin/com/server/dpmcore/member/member/application/exception/MemberExceptionCode.kt
@@ -12,10 +12,11 @@ enum class MemberExceptionCode(
     val message: String,
 ) : ExceptionCode {
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER-404-01", "멤버를 찾을 수 없습니다"),
-    MEMBER_ID_CAN_NOT_BE_NULL(HttpStatus.BAD_REQUEST, "MEMBER-400-02", "멤버 ID는 null일 수 없습니다"),
     INVALID_MEMBER_ID(HttpStatus.BAD_REQUEST, "MEMBER-400-01", "유효하지 않은 멤버 ID입니다"),
     COHORT_MEMBERS_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER-404-02", "기수에 속한 멤버를 찾을 수 없습니다"),
+    MEMBER_ID_REQUIRED(HttpStatus.BAD_REQUEST, "MEMBER-400-2", "멤버 ID는 null일 수 없습니다"),
     MEMBER_TEAM_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER-404-03", "멤버의 팀을 찾을 수 없습니다"),
+    MEMBER_NAME_AUTHORITY_REQUIRED(HttpStatus.BAD_REQUEST, "MEMBER-400-3", "멤버 이름과 권한은 null일 수 없습니다"),
     ;
 
     override fun getStatus(): HttpStatus = status
--- src/main/kotlin/com/server/dpmcore/member/member/domain/port/outbound/MemberPersistencePort.kt
+++ src/main/kotlin/com/server/dpmcore/member/member/domain/port/outbound/MemberPersistencePort.kt
@@ -3,6 +3,7 @@ package com.server.dpmcore.member.member.domain.port.outbound
 import com.server.dpmcore.authority.domain.model.AuthorityId
 import com.server.dpmcore.member.member.domain.model.Member
 import com.server.dpmcore.member.member.domain.model.MemberId
+import com.server.dpmcore.member.member.domain.port.inbound.query.MemberNameAuthorityQueryModel
 
 interface MemberPersistencePort {
     fun save(member: Member): Member
