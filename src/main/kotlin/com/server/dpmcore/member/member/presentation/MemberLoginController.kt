373e43bbc04546e59727ada111d4a54fa2e96a7e
leehaneum <leehaneum160924@kyonggi.ac.kr>
2025-08-01 16:50:33 +0900

    fix: 요청 도메인 쿠키에 저장

--- src/main/kotlin/com/server/dpmcore/member/member/presentation/MemberLoginController.kt
+++ src/main/kotlin/com/server/dpmcore/member/member/presentation/MemberLoginController.kt
@@ -1,14 +1,38 @@
 package com.server.dpmcore.member.member.presentation
 
+import jakarta.servlet.http.Cookie
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import java.net.URI
 
 @Controller
 class MemberLoginController {
     companion object {
         const val KAKAO_REDIRECT_URL = "redirect:/oauth2/authorization/kakao"
+        const val REQUEST_DOMAIN = "REQUEST_DOMAIN"
     }
 
     @GetMapping("/login/kakao")
-    fun login(): String = KAKAO_REDIRECT_URL
+    fun login(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+    ): String {
+        try {
+            val requestDomain =
+                URI(request.getHeader("Origin") ?: request.getHeader("Referer") ?: request.requestURL.toString()).host
+
+            Cookie(REQUEST_DOMAIN, requestDomain).apply {
+                path = "/"
+                maxAge = 60
+                isHttpOnly = true
+                secure = true
+                response.addCookie(this)
+            }
+        } catch (e: Exception) {
+            throw IllegalArgumentException("Cannot extract host from request source")
+        }
+        return KAKAO_REDIRECT_URL
+    }
 }
--- src/main/kotlin/com/server/dpmcore/security/oauth/handler/CustomAuthenticationSuccessHandler.kt
+++ src/main/kotlin/com/server/dpmcore/security/oauth/handler/CustomAuthenticationSuccessHandler.kt
@@ -4,12 +4,14 @@ import com.server.dpmcore.member.member.domain.port.inbound.HandleMemberLoginUse
 import com.server.dpmcore.security.oauth.domain.CustomOAuth2User
 import com.server.dpmcore.security.oauth.dto.LoginResult
 import com.server.dpmcore.security.oauth.token.JwtTokenInjector
+import jakarta.servlet.http.Cookie
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.security.core.Authentication
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler
 import org.springframework.stereotype.Component
-import java.net.URI
+
+private const val REQUEST_DOMAIN = "REQUEST_DOMAIN"
 
 @Component
 class CustomAuthenticationSuccessHandler(
@@ -22,28 +24,20 @@ class CustomAuthenticationSuccessHandler(
         authentication: Authentication,
     ) {
         resolveLoginResultFromAuthentication(
-            getRequestDomain(request),
+            request.cookies?.firstOrNull { it.name == REQUEST_DOMAIN }?.value ?: request.serverName,
             authentication,
         ).let { loginResult ->
-            println(getRequestDomain(request))
+            println("요청 도메인: ${request.cookies?.firstOrNull { it.name == REQUEST_DOMAIN }?.value}")
             loginResult.refreshToken?.let {
                 tokenInjector.injectRefreshToken(it, response)
             }
+            Cookie(REQUEST_DOMAIN, request.serverName)
+                .apply {
+                    path = "/"
+                    maxAge = 0
+                }.also { response.addCookie(it) }
             response.sendRedirect(loginResult.redirectUrl)
         }
-    }
-
-    private fun getRequestDomain(request: HttpServletRequest): String {
-        val raw =
-            request.getHeader("Origin")
-                ?: request.getHeader("Referer")
-                ?: request.requestURL.toString()
-
-        return try {
-            URI(raw).host
-        } catch (e: Exception) {
-            null
-        } ?: throw IllegalArgumentException("Cannot extract host from request source: $raw")
     }
 
     private fun resolveLoginResultFromAuthentication(
