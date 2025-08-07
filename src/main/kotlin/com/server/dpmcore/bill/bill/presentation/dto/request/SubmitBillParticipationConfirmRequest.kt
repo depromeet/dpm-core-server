19154760e4ac451784257d986e8553b365036478
junwon <67488973+wjdwnsdnjs13@users.noreply.github.com>
2025-08-07 12:57:48 +0900

    refactor : list가 명확하게 보이도록 수정

--- src/main/kotlin/com/server/dpmcore/bill/bill/presentation/dto/request/SubmitBillParticipationConfirmRequest.kt
+++ src/main/kotlin/com/server/dpmcore/bill/bill/presentation/dto/request/SubmitBillParticipationConfirmRequest.kt
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.media.Schema
 
 data class SubmitBillParticipationConfirmRequest(
     @field:Schema(
-        description = "회식 일련번호",
+        description = "회식 일련번호 목록",
         example = "[1, 2, 3]",
         requiredMode = Schema.RequiredMode.REQUIRED,
     )
