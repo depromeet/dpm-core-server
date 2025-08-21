0898483a81f28911d4144177a753e0ec9a2896f5
junwon <wjdwnsdnjs13@naver.com>
2025-08-22 00:02:16 +0900

    refactor : 코드 리뷰 반영(Null처리)

--- src/main/kotlin/com/server/dpmcore/gathering/gathering/application/GatheringQueryService.kt
+++ src/main/kotlin/com/server/dpmcore/gathering/gathering/application/GatheringQueryService.kt
@@ -2,6 +2,7 @@ package com.server.dpmcore.gathering.gathering.application
 
 import com.server.dpmcore.bill.bill.domain.model.BillId
 import com.server.dpmcore.bill.bill.domain.port.inbound.query.BillMemberIsInvitationSubmittedQueryModel
+import com.server.dpmcore.gathering.exception.GatheringException
 import com.server.dpmcore.gathering.gathering.domain.model.Gathering
 import com.server.dpmcore.gathering.gathering.domain.model.GatheringId
 import com.server.dpmcore.gathering.gathering.domain.model.query.SubmittedParticipantGathering
@@ -100,6 +101,9 @@ class GatheringQueryService(
 
     override fun getBillMemberSubmittedList(billId: BillId): List<BillMemberIsInvitationSubmittedQueryModel> {
         val gatheringIds = getAllGatheringIdsByBillId(billId)
-        return gatheringMemberQueryService.getQueryGatheringMemberIsInvitationSubmitted(gatheringIds.first())
+        return gatheringMemberQueryService.getQueryGatheringMemberIsInvitationSubmitted(
+            gatheringIds.firstOrNull()
+                ?: throw GatheringException.GatheringNotFoundException(),
+        )
     }
 }
