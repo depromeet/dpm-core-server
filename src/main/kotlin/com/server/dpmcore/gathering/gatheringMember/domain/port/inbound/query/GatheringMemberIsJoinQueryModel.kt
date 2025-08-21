963ab4ff73b67aa2e90d06e8b813aefa6615db6e
junwon <67488973+wjdwnsdnjs13@users.noreply.github.com>
2025-08-21 16:09:26 +0900

    refactor : Swagger Schema 추가

--- src/main/kotlin/com/server/dpmcore/gathering/gatheringMember/domain/port/inbound/query/GatheringMemberIsJoinQueryModel.kt
+++ src/main/kotlin/com/server/dpmcore/gathering/gatheringMember/domain/port/inbound/query/GatheringMemberIsJoinQueryModel.kt
@@ -1,13 +1,39 @@
 package com.server.dpmcore.gathering.gatheringMember.domain.port.inbound.query
 
 import com.fasterxml.jackson.annotation.JsonInclude
+import io.swagger.v3.oas.annotations.media.Schema
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class GatheringMemberIsJoinQueryModel(
+    @field:Schema(
+        description = "멤버 이름",
+        example = "정준원",
+        requiredMode = Schema.RequiredMode.REQUIRED,
+    )
     val name: String,
+    @field:Schema(
+        description = "팀 번호",
+        example = "6",
+        requiredMode = Schema.RequiredMode.REQUIRED,
+    )
     val teamNumber: Int,
+    @field:Schema(
+        description = "권한 이름",
+        example = "ORGANIZER",
+        requiredMode = Schema.RequiredMode.REQUIRED,
+    )
     val authority: String,
+    @field:Schema(
+        description = "파트",
+        example = "SERVER",
+        requiredMode = Schema.RequiredMode.REQUIRED,
+    )
     val part: String?,
+    @field:Schema(
+        description = "gathering 참여 여부",
+        example = "true",
+        requiredMode = Schema.RequiredMode.REQUIRED,
+    )
     val isJoined: Boolean,
 ) {
     companion object {
