dc67dc9d4f369a1a9573ddbf5b0288b5fabe5dee
junwon <wjdwnsdnjs13@naver.com>
2025-08-27 21:27:35 +0900

    feat : Swagger Schema 추가

--- src/main/kotlin/com/server/dpmcore/bill/bill/presentation/dto/request/UpdateMemberDepositRequest.kt
+++ src/main/kotlin/com/server/dpmcore/bill/bill/presentation/dto/request/UpdateMemberDepositRequest.kt
@@ -3,9 +3,20 @@ package com.server.dpmcore.bill.bill.presentation.dto.request
 import com.server.dpmcore.bill.bill.domain.model.BillId
 import com.server.dpmcore.bill.bill.domain.port.inbound.UpdateMemberDepositCommand
 import com.server.dpmcore.member.member.domain.model.MemberId
+import io.swagger.v3.oas.annotations.media.Schema
 
 data class UpdateMemberDepositRequest(
+    @field:Schema(
+        description = "입금 완료 여부",
+        example = "true",
+        requiredMode = Schema.RequiredMode.REQUIRED,
+    )
     val isDeposit: Boolean,
+    @field:Schema(
+        description = "상태 변경 메모",
+        example = "1차는 참여하고, 2차는 불참했습니다.",
+        requiredMode = Schema.RequiredMode.REQUIRED,
+    )
     val memo: String?,
 ) {
     fun toCommand(
--- src/main/kotlin/com/server/dpmcore/bill/bill/presentation/dto/request/UpdateMemberListDepositRequest.kt
+++ src/main/kotlin/com/server/dpmcore/bill/bill/presentation/dto/request/UpdateMemberListDepositRequest.kt
@@ -4,6 +4,7 @@ import com.server.dpmcore.bill.bill.domain.model.BillId
 import com.server.dpmcore.bill.bill.domain.port.inbound.UpdateMemberListDepositCommand
 import com.server.dpmcore.bill.bill.domain.port.inbound.UpdateMemberListDepositMemberCommand
 import com.server.dpmcore.member.member.domain.model.MemberId
+import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotEmpty
 
 data class UpdateMemberListDepositRequest(
@@ -11,7 +12,17 @@ data class UpdateMemberListDepositRequest(
     val members: List<UpdateMemberDepositRequest>,
 ) {
     data class UpdateMemberDepositRequest(
+        @field:Schema(
+            description = "멤버 식별자",
+            example = "1",
+            requiredMode = Schema.RequiredMode.REQUIRED,
+        )
         val memberId: MemberId,
+        @field:Schema(
+            description = "입금 완료 여부",
+            example = "true",
+            requiredMode = Schema.RequiredMode.REQUIRED,
+        )
         val isDeposit: Boolean,
     )
 
