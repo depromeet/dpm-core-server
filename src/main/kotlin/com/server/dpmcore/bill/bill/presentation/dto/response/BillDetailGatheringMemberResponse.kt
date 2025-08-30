0d5d9a3663c693dc4ccb766838d7592dce1e1833
junwon <wjdwnsdnjs13@naver.com>
2025-08-30 22:11:31 +0900

    refactor : 코드 리뷰 반영

--- src/main/kotlin/com/server/dpmcore/bill/bill/presentation/dto/response/BillDetailGatheringMemberResponse.kt
+++ src/main/kotlin/com/server/dpmcore/bill/bill/presentation/dto/response/BillDetailGatheringMemberResponse.kt
@@ -21,9 +21,10 @@ data class BillDetailGatheringMemberResponse(
     // TODO : 추후 이름 추가
 //    val name: String? = null,
     @field:Schema(
-        description = "회식 참여 여부",
+        description = "회식 참여 여부 (참여: true, 불참: false, 미응답: null)",
         example = "true",
-        requiredMode = Schema.RequiredMode.REQUIRED,
+        requiredMode = Schema.RequiredMode.NOT_REQUIRED,
+        nullable = true,
     )
     val isJoined: Boolean?,
     @field:Schema(
--- src/main/kotlin/com/server/dpmcore/bill/bill/presentation/dto/response/SubmittedGatheringParticipationResponse.kt
+++ src/main/kotlin/com/server/dpmcore/bill/bill/presentation/dto/response/SubmittedGatheringParticipationResponse.kt
@@ -1,9 +1,21 @@
 package com.server.dpmcore.bill.bill.presentation.dto.response
 
 import com.server.dpmcore.gathering.gathering.domain.model.GatheringId
+import io.swagger.v3.oas.annotations.media.Schema
 
 data class SubmittedGatheringParticipationResponse(
+    @field:Schema(
+        description = "회식 식별자",
+        example = "1",
+        requiredMode = Schema.RequiredMode.REQUIRED,
+    )
     val gatheringId: GatheringId,
+    @field:Schema(
+        description = "회식 참여 여부 (참여: true, 불참: false, 미응답: null)",
+        example = "1",
+        requiredMode = Schema.RequiredMode.NOT_REQUIRED,
+        nullable = true,
+    )
     val isJoined: Boolean?,
 ) {
     companion object {
