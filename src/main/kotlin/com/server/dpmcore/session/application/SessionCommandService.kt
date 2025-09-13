807d2cdeba3eafd85be82ae8c7bf29651569525d
leehaneum <leehaneum160924@kyonggi.ac.kr>
2025-09-13 22:49:09 +0900

    refactor: 세션 메서드 문서화

--- src/main/kotlin/com/server/dpmcore/session/application/SessionCommandService.kt
+++ src/main/kotlin/com/server/dpmcore/session/application/SessionCommandService.kt
@@ -33,6 +33,15 @@ class SessionCommandService(
         sessionPersistencePort.save(session)
     }
 
+    /**
+     * 출석 시작 시간이 세션과 같은 날짜인지 확인합니다.
+     *
+     * @param session 세션
+     * @param attendanceStartTime 출석 시작 시간
+     * @throws AttendanceStartTimeDateMismatchException 날짜가 다를 경우
+     * @author LeeHanEum
+     * @since 2025.09.13
+     */
     private fun checkIsSameDateAsSession(
         session: Session,
         attendanceStartTime: Instant,
