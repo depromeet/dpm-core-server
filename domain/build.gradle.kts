11a4cf5d84d17eaa5190944e29422f969657d751
leehaneum <leehaneum160924@kyonggi.ac.kr>
2025-11-02 18:05:27 +0900

    chore(dependencies): 도메인 모듈에 Kotest 프레임워크 의존성 추가

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,3 +1,13 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
 }
+
+dependencies {
+    testImplementation(libs.kotest.runner.junit5)
+    testImplementation(libs.kotest.assertions.core)
+    testImplementation(libs.kotest.property)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
