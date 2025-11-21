e37520cf23992a3e74cde1fd13588eda5ab01f4d
leehaneum <leehaneum160924@kyonggi.ac.kr>
2025-11-21 17:37:19 +0900

    refactor(session): 세션 이벤트 리스너에 예외 추적 가능하도록 로그 작성

--- application/src/main/kotlin/core/application/attendance/application/event/listener/SessionDeleteEventListener.kt
+++ application/src/main/kotlin/core/application/attendance/application/event/listener/SessionDeleteEventListener.kt
@@ -2,6 +2,7 @@ package core.application.attendance.application.event.listener
 
 import core.application.attendance.application.service.AttendanceCommandService
 import core.domain.session.event.SessionDeleteEvent
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import org.springframework.transaction.event.TransactionPhase
@@ -11,12 +12,24 @@ import org.springframework.transaction.event.TransactionalEventListener
 class SessionDeleteEventListener(
     private val attendanceCommandService: AttendanceCommandService,
 ) {
+    private val logger = KotlinLogging.logger { SessionDeleteEventListener::class.java }
+
     @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     fun handleSessionDeletion(event: SessionDeleteEvent) {
-        attendanceCommandService.deleteAttendancesBySessionId(
-            sessionId = event.sessionId,
-            deletedAt = event.deletedAt,
-        )
+        logger.info { "Handling SessionDeleteEvent for sessionId: ${event.sessionId}" }
+
+        try {
+            attendanceCommandService.deleteAttendancesBySessionId(
+                sessionId = event.sessionId,
+                deletedAt = event.deletedAt,
+            )
+        } catch (e: Exception) {
+            logger.error(e) {
+                "Error occurred while handling SessionDeleteEvent for sessionId: ${event.sessionId}. Caused By: ${e.cause}"
+            }
+        } finally {
+            logger.info { "Completed handling SessionDeleteEvent for sessionId: ${event.sessionId}" }
+        }
     }
 }
--- application/src/main/kotlin/core/application/attendance/application/event/listener/SessionUpdateEventListener.kt
+++ application/src/main/kotlin/core/application/attendance/application/event/listener/SessionUpdateEventListener.kt
@@ -2,6 +2,7 @@ package core.application.attendance.application.event.listener
 
 import core.application.attendance.application.service.AttendanceCommandService
 import core.domain.session.event.SessionUpdateEvent
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import org.springframework.transaction.event.TransactionPhase
@@ -11,13 +12,25 @@ import org.springframework.transaction.event.TransactionalEventListener
 class SessionUpdateEventListener(
     private val attendanceCommandService: AttendanceCommandService,
 ) {
+    private val logger = KotlinLogging.logger { SessionUpdateEventListener::class.java }
+
     @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     fun handleSessionUpdate(event: SessionUpdateEvent) {
-        attendanceCommandService.updateAttendancesByPolicy(
-            sessionId = event.sessionId,
-            lateStart = event.lateStart,
-            absentStart = event.absentStart,
-        )
+        logger.info { "Handling SessionUpdateEvent for sessionId: ${event.sessionId}" }
+
+        try {
+            attendanceCommandService.updateAttendancesByPolicy(
+                sessionId = event.sessionId,
+                lateStart = event.lateStart,
+                absentStart = event.absentStart,
+            )
+        } catch (e: Exception) {
+            logger.error(e) {
+                "Error occurred while handling SessionUpdateEvent for sessionId: ${event.sessionId}. Caused By: ${e.cause}"
+            }
+        } finally {
+            logger.info { "Completed handling SessionUpdateEvent for sessionId: ${event.sessionId}" }
+        }
     }
 }
