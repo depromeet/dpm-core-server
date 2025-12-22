e571507041b475064e12a1c79629a7b28a3eb1e9
leehaneum <leehaneum160924@kyonggi.ac.kr>
2025-12-22 12:01:44 +0900

    feat(authorization): 도메인 객체 주석 작성

--- domain/src/main/kotlin/core/domain/authorization/aggregate/RolePermission.kt
+++ domain/src/main/kotlin/core/domain/authorization/aggregate/RolePermission.kt
@@ -4,6 +4,12 @@ import core.domain.authorization.vo.PermissionId
 import core.domain.authorization.vo.RoleId
 import java.time.Instant
 
+/**
+ * @property roleId 권한이 부여된 Role의 식별자입니다.
+ * @property permissionId Role에 부여된 Permission의 식별자입니다.
+ * @property grantedAt 권한이 Role에 부여된 시각입니다.
+ * @property revokedAt 권한이 Role에서 회수된 시각입니다.
+ */
 class RolePermission (
     val roleId: RoleId,
     val permissionId: PermissionId,
