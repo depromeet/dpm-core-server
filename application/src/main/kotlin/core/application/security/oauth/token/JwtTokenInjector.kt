4730fdb60297738c4eb9b81747c297a4ecb2d68e
BlackBean99 <ymecca12@gmail.com>
2026-01-14 16:24:35 +0900

    [fix]: inject refresh token configuration change

--- application/src/main/kotlin/core/application/security/oauth/token/JwtTokenInjector.kt
+++ application/src/main/kotlin/core/application/security/oauth/token/JwtTokenInjector.kt
@@ -2,10 +2,8 @@ package core.application.security.oauth.token
 
 import core.application.security.properties.SecurityProperties
 import core.application.security.properties.TokenProperties
-import core.domain.refreshToken.aggregate.RefreshToken
 import jakarta.servlet.http.Cookie
 import jakarta.servlet.http.HttpServletResponse
-import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames
 import org.springframework.stereotype.Component
 
 @Component
@@ -13,6 +11,30 @@ class JwtTokenInjector(
     private val tokenProperties: TokenProperties,
     private val securityProperties: SecurityProperties,
 ) {
+    fun injectRefreshToken(
+            refreshToken: String,
+            response: HttpServletResponse,
+        ) {
+            val cookie = Cookie("refresh_token", refreshToken).apply {
+                isHttpOnly = true
+                secure = true
+                path = "/"
+                domain = securityProperties.cookie.domain
+                maxAge = 60 * 60 * 24 * 14
+                setSameSite(this, "None")
+            }
+
+            response.addCookie(cookie)
+        }
+
+    private fun setSameSite(cookie: Cookie, sameSite: String) {
+        // Servlet API는 SameSite 직접 지원 안 함 → 헤더로 강제
+        cookie.setAttribute("SameSite", sameSite)
+    }
+
+
+
+/* Temp deprecate 20260114
     fun injectRefreshToken(
         refreshToken: RefreshToken,
         response: HttpServletResponse,
@@ -24,24 +46,26 @@ class JwtTokenInjector(
             response,
         )
     }
+*/
 
-    fun addCookie(
-        name: String,
-        value: String,
-        maxAge: Int,
-        response: HttpServletResponse,
-    ) {
-        val cookie =
-            Cookie(name, value).apply {
-                path = "/"
-                this.maxAge = maxAge
-                isHttpOnly = securityProperties.cookie.httpOnly
-                domain = null // Host Only
-                secure = securityProperties.cookie.secure
-                setAttribute("SameSite", "None")
-            }
-        response.addCookie(cookie)
-    }
+/* Temp deprecate 20260114
+        fun addCookie(
+            name: String,
+            value: String,
+            maxAge: Int,
+            response: HttpServletResponse,
+        ) {
+            val cookie =
+                Cookie(name, value).apply {
+                    path = "/"
+                    this.maxAge = maxAge
+                    isHttpOnly = securityProperties.cookie.httpOnly
+                    domain = null // Host Only
+                    secure = securityProperties.cookie.secure
+                    setAttribute("SameSite", "None")
+                }
+            response.addCookie(cookie)
+        }*/
 
     fun invalidateCookie(
         name: String,
