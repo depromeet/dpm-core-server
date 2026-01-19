7ea7805f5a610ff12b095d5f7d89ddf42db819e0
BlackBean99 <ymecca12@gmail.com>
2026-01-19 23:49:45 +0900

    [refactor] : implement getRoleIdByCode

--- application/src/main/kotlin/core/application/authorization/application/service/RoleQueryService.kt
+++ application/src/main/kotlin/core/application/authorization/application/service/RoleQueryService.kt
@@ -3,6 +3,7 @@ package core.application.authorization.application.service
 import core.domain.authorization.aggregate.Role
 import core.domain.authorization.port.inbound.RoleQueryUseCase
 import core.domain.authorization.port.outbound.RolePersistencePort
+import core.domain.authorization.vo.RoleId
 import core.domain.member.vo.MemberId
 import org.springframework.stereotype.Service
 
@@ -21,4 +22,7 @@ class RoleQueryService(
 
     override fun getPermissionsByMemberId(memberId: MemberId): List<String> =
         rolePersistencePort.findAllPermissionsByMemberId(memberId)
+
+    override fun getRoleIdByCode(code: String): RoleId =
+        rolePersistencePort.getRoleIdByCode(code)
 }
