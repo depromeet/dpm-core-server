687f39d3ff4b3efa45ff8807f90c63da763c8945
junwon <67488973+wjdwnsdnjs13@users.noreply.github.com>
2026-01-23 16:28:15 +0900

    refactor : ktlint 적용

--- persistence/src/main/kotlin/core/persistence/firebase/FirebaseNotificationManager.kt
+++ persistence/src/main/kotlin/core/persistence/firebase/FirebaseNotificationManager.kt
@@ -7,24 +7,30 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 class FirebaseNotificationManager {
+    fun sendPushNotification(
+        token: String,
+        title: String,
+        messageContent: String,
+    ): Boolean {
+        val notification: Notification =
+            Notification.builder()
+                .setTitle(title)
+                .setBody(messageContent)
+                .build()
 
-    fun sendPushNotification(token: String, title: String, messageContent: String): Boolean {
-        val notification: Notification = Notification.builder()
-            .setTitle(title)
-            .setBody(messageContent)
-            .build()
+        val message: Message =
+            Message.builder()
+                .setToken(token)
+                .setNotification(notification)
+                .build()
 
-        val message: Message = Message.builder()
-            .setToken(token)
-            .setNotification(notification)
-            .build()
-
-        val send: String? = try {
-            FirebaseMessaging.getInstance().send(message)
-        } catch (e: Exception) {
-            e.printStackTrace()
-            null
-        }
+        val send: String? =
+            try {
+                FirebaseMessaging.getInstance().send(message)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                null
+            }
 
         return send != null
     }
