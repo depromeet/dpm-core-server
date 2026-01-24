9c5bb4b4ceee88513a9239a341f2e1de288dcbcd
junwon <wjdwnsdnjs13@naver.com>
2026-01-24 23:07:26 +0900

    refactor : 오류 수정

--- application/src/main/kotlin/core/application/notification/presentation/controller/NotificationController.kt
+++ application/src/main/kotlin/core/application/notification/presentation/controller/NotificationController.kt
@@ -4,6 +4,7 @@ import core.application.common.exception.CustomResponse
 import core.application.notification.application.service.NotificationService
 import core.application.notification.presentation.request.NotificationRequest
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -13,13 +14,14 @@ class NotificationController(
     val notificationService: NotificationService,
 ) : NotificationApi {
     @PostMapping
-    override fun testSendNotification(notificationRequest: NotificationRequest): CustomResponse<Void> {
-        val messageId: Boolean =
-            notificationService.sendPushNotification(
-                notificationRequest.message,
-                notificationRequest.title,
-                notificationRequest.message,
-            )
+    override fun testSendNotification(
+        @RequestBody notificationRequest: NotificationRequest,
+    ): CustomResponse<Void> {
+        notificationService.sendPushNotification(
+            notificationRequest.message,
+            notificationRequest.title,
+            notificationRequest.message,
+        )
         return CustomResponse.ok()
     }
 }
