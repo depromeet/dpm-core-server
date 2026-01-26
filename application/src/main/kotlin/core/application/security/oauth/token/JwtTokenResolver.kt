086bd236bb8493aa2a949b885b567b752ff1c62f
junwon <67488973+wjdwnsdnjs13@users.noreply.github.com>
2026-01-26 23:11:52 +0900

    fix : refreshToken 카멜 케이스 통일 (#254)

--- application/src/main/kotlin/core/application/security/oauth/token/JwtTokenResolver.kt
+++ application/src/main/kotlin/core/application/security/oauth/token/JwtTokenResolver.kt
@@ -1,19 +1,19 @@
 package core.application.security.oauth.token
 
+import core.application.security.oauth.token.JwtTokenConstant.REFRESH_TOKEN_CAMEL_CASE
 import jakarta.servlet.http.HttpServletRequest
-import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames.REFRESH_TOKEN
 import org.springframework.stereotype.Component
 
 @Component
 class JwtTokenResolver {
-    fun resolveRefreshTokenFromRequest(request: HttpServletRequest): String? = resolveFromCookie(request, REFRESH_TOKEN)
+    fun resolveRefreshTokenFromRequest(request: HttpServletRequest): String? =
+        resolveFromCookie(request, REFRESH_TOKEN_CAMEL_CASE)
 
     private fun resolveFromCookie(
         request: HttpServletRequest,
         cookieName: String,
-    ): String? {
-        return request.cookies
+    ): String? =
+        request.cookies
             ?.firstOrNull { it.name == cookieName }
             ?.value
-    }
 }
