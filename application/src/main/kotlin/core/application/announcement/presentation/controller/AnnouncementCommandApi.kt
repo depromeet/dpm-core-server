5a7d0568e0273c62ceb185d267d1bf31415b0a20
junwon <67488973+wjdwnsdnjs13@users.noreply.github.com>
2026-01-30 16:18:39 +0900

    feat : 공지/과제 작성 API 구현

--- application/src/main/kotlin/core/application/announcement/presentation/controller/AnnouncementCommandApi.kt
+++ application/src/main/kotlin/core/application/announcement/presentation/controller/AnnouncementCommandApi.kt
@@ -1,3 +1,24 @@
 package core.application.announcement.presentation.controller
 
-interface AnnouncementCommandApi
+import core.application.announcement.presentation.request.CreateAnnouncementRequest
+import core.application.common.exception.CustomResponse
+import core.domain.member.vo.MemberId
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
+
+@Tag(name = "Announcement", description = "공지/과제 API")
+interface AnnouncementCommandApi {
+    @ApiResponse(
+        responseCode = "200",
+        description = "공지/과제 추가 성공",
+    )
+    @Operation(
+        summary = "공지/과제 추가 API",
+        description = "공지나 과제를 작성하는 API입니다",
+    )
+    fun createAnnouncement(
+        createAnnouncementRequest: CreateAnnouncementRequest,
+        memberId: MemberId,
+    ): CustomResponse<Void>
+}
--- application/src/main/kotlin/core/application/announcement/presentation/controller/AnnouncementCommandController.kt
+++ application/src/main/kotlin/core/application/announcement/presentation/controller/AnnouncementCommandController.kt
@@ -1,6 +1,39 @@
 package core.application.announcement.presentation.controller
 
+import core.application.announcement.application.service.AnnouncementCommandService
+import core.application.announcement.presentation.request.CreateAnnouncementRequest
+import core.application.common.exception.CustomResponse
+import core.application.security.annotation.CurrentMemberId
+import core.domain.member.vo.MemberId
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class AnnouncementCommandController : AnnouncementCommandApi
+@RequestMapping("/v1/announcements")
+class AnnouncementCommandController(
+    val announcementCommandService: AnnouncementCommandService,
+) : AnnouncementCommandApi {
+    @PostMapping
+    override fun createAnnouncement(
+        @RequestBody
+        createAnnouncementRequest: CreateAnnouncementRequest,
+        @CurrentMemberId
+        memberId: MemberId,
+    ): CustomResponse<Void> {
+        announcementCommandService.create(
+            authorId = memberId,
+            announcementType = createAnnouncementRequest.announcementType,
+            submitType = createAnnouncementRequest.submitType,
+            title = createAnnouncementRequest.title,
+            content = createAnnouncementRequest.content,
+            submitLink = createAnnouncementRequest.submitLink,
+            startAt = createAnnouncementRequest.startAt,
+            dueAt = createAnnouncementRequest.dueAt,
+            scheduledAt = createAnnouncementRequest.scheduledAt,
+            shouldSendNotification = createAnnouncementRequest.shouldSendNotification,
+        )
+        return CustomResponse.ok()
+    }
+}
