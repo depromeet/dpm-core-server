054d12c9f356bf8ea1243dfe0cf9a79d0d679905
junwon <wjdwnsdnjs13@naver.com>
2026-02-01 00:17:45 +0900

    feat : announcementId로 과제 찾기 추가

--- application/src/main/kotlin/core/application/announcement/application/service/AssignmentQueryService.kt
+++ application/src/main/kotlin/core/application/announcement/application/service/AssignmentQueryService.kt
@@ -3,6 +3,7 @@ package core.application.announcement.application.service
 import core.domain.announcement.aggregate.Assignment
 import core.domain.announcement.port.inbound.AssignmentQueryUseCase
 import core.domain.announcement.port.outbound.AssignmentPersistencePort
+import core.domain.announcement.vo.AnnouncementId
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -12,4 +13,7 @@ class AssignmentQueryService(
     val assignmentPersistencePort: AssignmentPersistencePort,
 ) : AssignmentQueryUseCase {
     override fun getAllAssignments(): List<Assignment> = assignmentPersistencePort.findAll()
+
+    override fun getAssignmentByAnnouncementId(announcementId: AnnouncementId): Assignment =
+        assignmentPersistencePort.findByAnnouncementId(announcementId)
 }
--- domain/src/main/kotlin/core/domain/announcement/port/outbound/AssignmentPersistencePort.kt
+++ domain/src/main/kotlin/core/domain/announcement/port/outbound/AssignmentPersistencePort.kt
@@ -1,9 +1,12 @@
 package core.domain.announcement.port.outbound
 
 import core.domain.announcement.aggregate.Assignment
+import core.domain.announcement.vo.AnnouncementId
 
 interface AssignmentPersistencePort {
     fun save(assignment: Assignment): Assignment
 
     fun findAll(): List<Assignment>
+
+    fun findByAnnouncementId(announcementId: AnnouncementId): Assignment
 }
