a8793804e43406461d78404a0be0f6f5c8104e23
junwon <wjdwnsdnjs13@naver.com>
2026-02-01 10:20:54 +0900

    feat : 공지/과제 상세 정보 조회 API 구현

--- application/src/main/kotlin/core/application/announcement/presentation/controller/AnnouncementQueryApi.kt
+++ application/src/main/kotlin/core/application/announcement/presentation/controller/AnnouncementQueryApi.kt
@@ -1,7 +1,9 @@
 package core.application.announcement.presentation.controller
 
+import core.application.announcement.presentation.response.AnnouncementDetailResponse
 import core.application.announcement.presentation.response.AnnouncementListResponse
 import core.application.common.exception.CustomResponse
+import core.domain.announcement.vo.AnnouncementId
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.ExampleObject
@@ -52,4 +54,45 @@ interface AnnouncementQueryApi {
         description = "공지/과제 목록을 조회합니다",
     )
     fun getAnnouncementList(): CustomResponse<AnnouncementListResponse>
+
+    @ApiResponse(
+        responseCode = "200",
+        description = "공지/과제 상세",
+        content = [
+            Content(
+                mediaType = APPLICATION_JSON_VALUE,
+                schema = Schema(implementation = CustomResponse::class),
+                examples = [
+                    ExampleObject(
+                        name = "공지/과제 상세 조회 성공 응답",
+                        value = """
+                            {
+                              "status": "OK",
+                              "message": "요청에 성공했습니다",
+                              "code": "GLOBAL-200-01",
+                              "data": {
+                                "announcementCount": 1,
+                                "announcements": [
+                                  {
+                                    "announcementId": 3,
+                                    "title": "공지/과제 테스트용 제목 001",
+                                    "announcementType": "ASSIGNMENT",
+                                    "assignmentType": "INDIVIDUAL",
+                                    "createdAt": "2026-01-30T07:15:36.977721",
+                                    "readMemberCount": 0
+                                  }
+                                ]
+                              }
+                            }
+                    """,
+                    ),
+                ],
+            ),
+        ],
+    )
+    @Operation(
+        summary = "공지/과제 상세 조회 API",
+        description = "공지/과제 상세 내용을 조회합니다",
+    )
+    fun getAnnouncementDetail(announcementId: AnnouncementId): CustomResponse<AnnouncementDetailResponse>
 }
--- application/src/main/kotlin/core/application/announcement/presentation/controller/AnnouncementQueryController.kt
+++ application/src/main/kotlin/core/application/announcement/presentation/controller/AnnouncementQueryController.kt
@@ -1,10 +1,13 @@
 package core.application.announcement.presentation.controller
 
 import core.application.announcement.application.service.AnnouncementQueryService
+import core.application.announcement.presentation.response.AnnouncementDetailResponse
 import core.application.announcement.presentation.response.AnnouncementListResponse
 import core.application.common.exception.CustomResponse
+import core.domain.announcement.vo.AnnouncementId
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -15,4 +18,10 @@ class AnnouncementQueryController(
     @GetMapping
     override fun getAnnouncementList(): CustomResponse<AnnouncementListResponse> =
         CustomResponse.ok(announcementQueryService.getAllAnnouncements())
+
+    @GetMapping("/{announcementId}")
+    override fun getAnnouncementDetail(
+        @RequestParam announcementId: AnnouncementId,
+    ): CustomResponse<AnnouncementDetailResponse> =
+        CustomResponse.ok(announcementQueryService.getAnnouncementDetail(announcementId))
 }
