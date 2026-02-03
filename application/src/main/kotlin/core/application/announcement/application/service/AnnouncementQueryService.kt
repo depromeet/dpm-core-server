1005f47fd464d011ee5b3983fd78ddb8648e7fa5
junwon <wjdwnsdnjs13@naver.com>
2026-02-03 22:08:05 +0900

    feat : 멤버별 공지/회식 읽음 여부 목록 조회 비즈니스 로직 구현

--- application/src/main/kotlin/core/application/announcement/application/service/AnnouncementQueryService.kt
+++ application/src/main/kotlin/core/application/announcement/application/service/AnnouncementQueryService.kt
@@ -3,6 +3,7 @@ package core.application.announcement.application.service
 import core.application.announcement.application.exception.AnnouncementNotFoundException
 import core.application.announcement.presentation.response.AnnouncementDetailResponse
 import core.application.announcement.presentation.response.AnnouncementListResponse
+import core.application.announcement.presentation.response.AnnouncementViewMemberListItemResponse
 import core.application.announcement.presentation.response.AnnouncementViewMemberListResponse
 import core.application.session.presentation.mapper.TimeMapper.instantToLocalDateTime
 import core.domain.announcement.aggregate.Announcement
@@ -12,6 +13,10 @@ import core.domain.announcement.port.inbound.AnnouncementReadQueryUseCase
 import core.domain.announcement.port.outbound.AnnouncementPersistencePort
 import core.domain.announcement.port.outbound.query.AnnouncementListItemQueryModel
 import core.domain.announcement.vo.AnnouncementId
+import core.domain.member.aggregate.Member
+import core.domain.member.port.inbound.MemberQueryUseCase
+import core.domain.member.vo.MemberId
+import core.domain.team.vo.TeamId
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -20,6 +25,7 @@ import org.springframework.transaction.annotation.Transactional
 class AnnouncementQueryService(
     val announcementPersistencePort: AnnouncementPersistencePort,
     val announcementReadQueryUseCase: AnnouncementReadQueryUseCase,
+    val memberQueryUseCase: MemberQueryUseCase,
 ) : AnnouncementQueryUseCase {
     fun getAllAnnouncements(): AnnouncementListResponse {
         val announcementListItemQueryModels: List<AnnouncementListItemQueryModel> =
@@ -45,14 +51,29 @@ class AnnouncementQueryService(
     }
 
     fun getAnnouncementReadMemberList(announcementId: AnnouncementId): AnnouncementViewMemberListResponse {
-        val announcement: Announcement = getAnnouncementById(announcementId)
-        val announcementReads: List<AnnouncementRead> = a
+        val announcementReads: List<AnnouncementRead> = announcementReadQueryUseCase.getByAnnouncementId(announcementId)
 
-        return AnnouncementDetailResponse.of(
-            title = announcement.title,
-            content = announcement.content,
-            createdAt = instantToLocalDateTime(announcement.createdAt!!),
-            readMemberNames = readMemberNames,
+        val readMemberIds: List<MemberId> =
+            announcementReads.filter { it.isRead() }.map { it.memberId }
+        val readMembers: List<Member> = memberQueryUseCase.getMembersByIds(readMemberIds)
+        val readMemberItems: List<AnnouncementViewMemberListItemResponse> =
+            readMembers.map { readMember ->
+                val teamId: TeamId = memberQueryUseCase.getMemberTeamId(readMember.id!!)
+                AnnouncementViewMemberListItemResponse.of(readMember, teamId)
+            }
+
+        val unreadMemberIds: List<MemberId> =
+            announcementReads.filter { !it.isRead() }.map { it.memberId }
+        val unreadMembers: List<Member> = memberQueryUseCase.getMembersByIds(unreadMemberIds)
+        val unreadMemberItems: List<AnnouncementViewMemberListItemResponse> =
+            unreadMembers.map { unreadMember ->
+                val teamId: TeamId = memberQueryUseCase.getMemberTeamId(unreadMember.id!!)
+                AnnouncementViewMemberListItemResponse.of(unreadMember, teamId)
+            }
+
+        return AnnouncementViewMemberListResponse.of(
+            readMembers = readMemberItems,
+            unreadMembers = unreadMemberItems,
         )
     }
 }
