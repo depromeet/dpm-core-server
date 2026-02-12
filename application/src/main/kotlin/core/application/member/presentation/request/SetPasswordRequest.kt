3a2aa6896de76406af9c78ff38a5fb38ee71de48
BlackBean99 <ymecca12@gmail.com>
2026-02-12 20:57:00 +0900

    [feat] : add Missing bean

---
+++ application/src/main/kotlin/core/application/member/presentation/request/SetPasswordRequest.kt
@@ -0,0 +1,16 @@
+package core.application.member.presentation.request
+
+import jakarta.validation.constraints.NotBlank
+
+/**
+ * 이메일 비밀번호 설정/변경 요청 DTO입니다.
+ */
+data class SetPasswordRequest(
+    @field:NotBlank(message = "비밀번호는 필수입니다")
+    val newPassword: String,
+
+    @field:NotBlank(message = "비밀번호 확인은 필수입니다")
+    val confirmPassword: String,
+
+    val oldPassword: String? = null,  // 기존 비밀번호가 있는 경우에만 필요
+)
