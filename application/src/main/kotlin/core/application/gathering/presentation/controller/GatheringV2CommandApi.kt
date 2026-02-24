28e8bfddefcc67740ae2325f67e6b850f950feab
junwon <67488973+wjdwnsdnjs13@users.noreply.github.com>
2026-02-24 20:23:15 +0900

    feat : 회식 업데이트 API 구현

--- application/src/main/kotlin/core/application/gathering/presentation/controller/GatheringV2CommandApi.kt
+++ application/src/main/kotlin/core/application/gathering/presentation/controller/GatheringV2CommandApi.kt
@@ -3,6 +3,8 @@ package core.application.gathering.presentation.controller
 import core.application.common.exception.CustomResponse
 import core.application.gathering.presentation.request.CreateGatheringV2ByInviteTagNamesRequest
 import core.application.gathering.presentation.request.CreateGatheringV2Request
+import core.application.gathering.presentation.request.UpdateGatheringV2Request
+import core.domain.gathering.vo.GatheringV2Id
 import core.domain.member.vo.MemberId
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
@@ -12,11 +14,11 @@ import io.swagger.v3.oas.annotations.tags.Tag
 interface GatheringV2CommandApi {
     @ApiResponse(
         responseCode = "200",
-        description = "회식 초대 추가 성공",
+        description = "회식 참여 조사 추가 성공",
     )
     @Operation(
-        summary = "회식 초대 추가 API",
-        description = "회식 초대를 추가하는 API입니다",
+        summary = "회식 참여 조사 생성 API",
+        description = "회식 참여 조사를 생성하는 API입니다",
     )
     fun createGatheringV2(
         createGatheringV2Request: CreateGatheringV2Request,
@@ -34,5 +36,19 @@ interface GatheringV2CommandApi {
     fun createGatheringV2ByInviteTagNames(
         createGatheringV2ByInviteTagNamesRequest: CreateGatheringV2ByInviteTagNamesRequest,
         memberId: MemberId,
+    ): CustomResponse<Void>
+
+    @ApiResponse(
+        responseCode = "200",
+        description = "회식 참여 조사 변경 성공",
+    )
+    @Operation(
+        summary = "회식 참여 조사 업데이트 API",
+        description = "회식 참여 조사를 업데이트하는 API입니다",
+    )
+    fun updateGatheringV2(
+        updateGatheringV2Request: UpdateGatheringV2Request,
+        memberId: MemberId,
+        gatheringId: GatheringV2Id,
     ): CustomResponse<Void>
 }
---
+++ application/src/main/kotlin/core/application/gathering/presentation/request/UpdateGatheringV2Request.kt
@@ -0,0 +1,32 @@
+package core.application.gathering.presentation.request
+
+import core.application.common.converter.TimeMapper.localDateTimeToInstant
+import core.domain.gathering.aggregate.GatheringV2
+import core.domain.gathering.enums.GatheringCategory
+import core.domain.gathering.vo.GatheringV2Id
+import core.domain.member.vo.MemberId
+import java.time.LocalDateTime
+
+data class UpdateGatheringV2Request(
+    val title: String,
+    val description: String,
+    val scheduledAt: LocalDateTime,
+    val closedAt: LocalDateTime,
+    val isApproved: Boolean,
+    val authorMemberId: MemberId,
+    val canEditAfterApproval: Boolean,
+    val inviteTags: List<CreateGatheringV2InviteTagRequest>,
+) {
+    fun toDomain(gatheringV2Id: GatheringV2Id): GatheringV2 =
+        GatheringV2(
+            id = gatheringV2Id,
+            title = title,
+            description = description,
+            category = GatheringCategory.GATHERING,
+            scheduledAt = localDateTimeToInstant(scheduledAt),
+            closedAt = localDateTimeToInstant(closedAt),
+            isApproved = isApproved,
+            authorMemberId = authorMemberId,
+            canEditAfterApproval = canEditAfterApproval,
+        )
+}
