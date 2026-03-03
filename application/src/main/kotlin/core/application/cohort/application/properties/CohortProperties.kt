aac18f36298a87b5d3fed13ba99410a560198413
junwon <wjdwnsdnjs13@naver.com>
2026-03-03 18:19:41 +0900

    refactor : 최신 기수 변경

--- application/src/main/kotlin/core/application/cohort/application/properties/CohortProperties.kt
+++ application/src/main/kotlin/core/application/cohort/application/properties/CohortProperties.kt
@@ -7,5 +7,5 @@ import org.springframework.context.annotation.Configuration
 @ConfigurationProperties(prefix = "cohort")
 data class CohortProperties(
     // TODO : 기수가 변경될 경우 수정 필요
-    var value: String = "17",
+    var value: String = "18",
 )
