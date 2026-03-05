5d57e452f911b26f2bfd367337181ee108c8b92a
junwon <wjdwnsdnjs13@naver.com>
2026-03-05 18:38:19 +0900

    refactor : 현재 기수 세션으로 변경

--- domain/src/main/kotlin/core/domain/session/port/outbound/SessionPersistencePort.kt
+++ domain/src/main/kotlin/core/domain/session/port/outbound/SessionPersistencePort.kt
@@ -6,7 +6,7 @@ import java.time.Instant
 interface SessionPersistencePort {
     fun findNextSessionBy(startOfToday: Instant): Session?
 
-    fun findAllSessions(cohortId: Long): List<Session>
+    fun findAllCohortSessions(cohortId: Long): List<Session>
 
     fun findSessionById(sessionId: Long): Session?
 
