6f5e82d82ea755d68e1c0f57bf013561cc227691
BlackBean99 <ymecca12@gmail.com>
2026-03-08 23:45:35 +0900

    [refactor]: convert whitelist request multi request support

--- application/src/main/kotlin/core/application/member/presentation/request/WhiteListCheckRequest.kt
+++ application/src/main/kotlin/core/application/member/presentation/request/WhiteListCheckRequest.kt
@@ -1,6 +1,35 @@
 package core.application.member.presentation.request
 
+import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.Valid
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotEmpty
+
 data class WhiteListCheckRequest(
-    val name: String,
-    val signupEmail: String,
-)
+    @field:Valid
+    @field:NotEmpty
+    @field:Schema(
+        description = "화이트리스트 체크 대상 멤버 목록",
+        requiredMode = Schema.RequiredMode.REQUIRED,
+    )
+    val members: List<MemberWhiteListCheckRequest>,
+) {
+    data class MemberWhiteListCheckRequest(
+        @field:NotBlank
+        @field:Schema(
+            description = "멤버 이름",
+            example = "홍길동",
+            requiredMode = Schema.RequiredMode.REQUIRED,
+        )
+        val name: String,
+        @field:NotBlank
+        @field:Email
+        @field:Schema(
+            description = "가입 이메일",
+            example = "user@example.com",
+            requiredMode = Schema.RequiredMode.REQUIRED,
+        )
+        val signupEmail: String,
+    )
+}
