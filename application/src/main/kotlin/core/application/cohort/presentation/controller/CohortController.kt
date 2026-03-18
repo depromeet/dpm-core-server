4b5241342a8e409e46b32576c5b0ae767abfaa91
BlackBean99 <54030889+BlackBean99@users.noreply.github.com>
2026-03-18 22:45:21 +0900

    refactor : Session url open and Cohort value adding (#425)

--- application/src/main/kotlin/core/application/cohort/presentation/controller/CohortController.kt
+++ application/src/main/kotlin/core/application/cohort/presentation/controller/CohortController.kt
@@ -3,6 +3,7 @@ package core.application.cohort.presentation.controller
 import core.application.cohort.application.service.CohortQueryService
 import core.application.cohort.presentation.response.CohortNumberResponse
 import core.application.common.exception.CustomResponse
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -10,9 +11,15 @@ import org.springframework.web.bind.annotation.RestController
 class CohortController(
     private val cohortQueryService: CohortQueryService,
 ) : CohortApi {
+    @PreAuthorize("permitAll()")
     @GetMapping("/v1/cohort")
     override fun latestCohort(): CustomResponse<CohortNumberResponse> {
-        val cohort = cohortQueryService.getLatestCohortValue()
-        return CustomResponse.ok(CohortNumberResponse(cohort))
+        val cohort = cohortQueryService.getLatestCohort()
+        return CustomResponse.ok(
+            CohortNumberResponse(
+                cohortId = cohort.id?.value ?: error("Latest cohort id must not be null"),
+                cohortNumber = cohort.value,
+            ),
+        )
     }
 }
