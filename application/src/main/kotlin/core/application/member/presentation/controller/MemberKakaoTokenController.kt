553e6e911c2923f642e19088a2c94fd43df02a9f
BlackBean99 <ymecca12@gmail.com>
2026-05-21 22:14:58 +0900

    function : memberKakaoTokenController build error

--- application/src/main/kotlin/core/application/member/presentation/controller/MemberKakaoTokenController.kt
+++ application/src/main/kotlin/core/application/member/presentation/controller/MemberKakaoTokenController.kt
@@ -2,13 +2,13 @@ package core.application.member.presentation.controller
 
 import core.application.common.exception.CustomResponse
 import core.application.member.application.service.auth.KakaoLoginTokenSaveService
-import core.application.member.presentation.request.KakaoLoginTokenSaveRequest
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.servlet.http.HttpServletResponse
 import jakarta.validation.Valid
+import jakarta.validation.constraints.NotBlank
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -37,4 +37,11 @@ class MemberKakaoTokenController(
         kakaoLoginTokenSaveService.save(request.accessToken, request.refreshToken, response)
         return CustomResponse.ok()
     }
+
+    data class KakaoLoginTokenSaveRequest(
+        @field:NotBlank(message = "accessToken은 필수입니다")
+        val accessToken: String,
+        @field:NotBlank(message = "refreshToken은 필수입니다")
+        val refreshToken: String,
+    )
 }
