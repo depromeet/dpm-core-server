34e290775087a1822f1738f9e6698c44b0a3d95e
BlackBean99 <54030889+BlackBean99@users.noreply.github.com>
2026-06-30 11:26:01 +0900

    Hotfix kakao whitelabel (#533)
    * hotfix : whitelabel of lazy initiation
    
    * hotfix : reissue POST -> GET

--- application/src/main/kotlin/core/application/refreshToken/presentation/controller/ReissueApi.kt
+++ application/src/main/kotlin/core/application/refreshToken/presentation/controller/ReissueApi.kt
@@ -7,8 +7,8 @@ import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.ExampleObject
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
-import io.swagger.v3.oas.annotations.parameters.RequestBody
 import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 
@@ -16,27 +16,7 @@ import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 interface ReissueApi {
     @Operation(
         summary = "액세스 토큰 발급 API",
-        description = "요청 본문의 refreshToken을 기반으로 액세스 토큰을 재발급합니다.",
-        requestBody =
-            RequestBody(
-                required = true,
-                content = [
-                    Content(
-                        mediaType = APPLICATION_JSON_VALUE,
-                        schema = Schema(implementation = RefreshTokenReissueRequest::class),
-                        examples = [
-                            ExampleObject(
-                                name = "액세스 토큰 발급 요청 예시",
-                                value = """
-                                    {
-                                        "refreshToken": "eyJhbGciOiJIUzI1NiJ9..."
-                                    }
-                                """,
-                            ),
-                        ],
-                    ),
-                ],
-            ),
+        description = "요청 쿠키의 refreshToken을 기반으로 액세스 토큰을 재발급합니다.",
     )
     @ApiResponse(
         responseCode = "200",
@@ -65,11 +45,7 @@ interface ReissueApi {
         ],
     )
     fun reissue(
-        request: RefreshTokenReissueRequest,
+        request: HttpServletRequest,
         response: HttpServletResponse,
     ): CustomResponse<TokenResponse>
 }
-
-data class RefreshTokenReissueRequest(
-    val refreshToken: String,
-)
